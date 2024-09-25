52606f3eba6a230adbf497128dcd1c8c71c08cfc
Christoph <christoph@rustige.me>
2024-09-25 21:29:05 +0200

    Implemented Wrapper Methods

---
+++ lib/Formatting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaleidolib.lib
+{
+    public class Formatting
+    {
+        private static string reset = TextStyle.reset;
+
+        public static string bold(string formatString)
+        {
+            return TextStyle.bold + formatString + reset;
+        }
+
+        public static string italic(string formatString)
+        {
+            return TextStyle.italic + formatString + reset;
+        }
+
+        public static string dim(string formatString)
+        {
+            return TextStyle.dim + formatString + reset;
+        }
+
+        public static string slowblink(string formatString)
+        {
+            return TextStyle.slowblink + formatString + reset;
+        }
+
+        public static string rapidblink(string formatString)
+        {
+            return TextStyle.rapidblink + formatString + reset;
+        }
+
+        public static string reverse(string formatString)
+        {
+            return TextStyle.reverse + formatString + reset;
+        }
+
+        public static string invisible(string formatString)
+        {
+            return TextStyle.invisible + formatString + reset;
+        }
+    }
+}
---
+++ lib/Lining.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaleidolib.lib
+{
+    public class Lining
+    {
+        private static string reset = TextStyle.reset;
+
+        public static string underline(string liningString)
+        {
+            return TextStyle.underline + liningString + reset;
+        }
+
+        public static string thickunderline(string liningString)
+        {
+            return TextStyle.thickunderline + liningString + reset;
+        }
+
+        public static string italicunderline(string liningString)
+        {
+            return TextStyle.italicunderline + liningString + reset;
+        }
+
+        public static string strikethrough(string liningString)
+        {
+            return TextStyle.strikethrough + liningString + reset;
+        }
+
+        public static string overline(string liningString)
+        {
+            return TextStyle.overline + liningString + reset;
+        }
+    }
+}
