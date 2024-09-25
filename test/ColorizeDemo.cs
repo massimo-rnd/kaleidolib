a969e1cb45213416d2811c6805fc3962ad6ef781
Christoph <christoph@rustige.me>
2024-09-25 20:51:21 +0200

    Initial Project Setup

---
+++ test/ColorizeDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaleidolib.test
+{
+    internal class ColorizeDemo
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
