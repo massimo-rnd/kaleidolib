fb9e2fd8296fa300893aea679f73f15021fb1a1d
Christoph <christoph@rustige.me>
2024-09-25 21:16:15 +0200

    Implemented TextStyles + Minor BackgroundColor changes

--- lib/BackgroundColor.cs
+++ lib/BackgroundColor.cs
@@ -8,13 +8,14 @@ namespace kaleidolib.lib
 {
     public class BackgroundColor
     {
-        public static String black = "\x1B[40m";
-        public static String red = "\x1B[41m";
-        public static String green = "\x1B[42m";
-        public static String yellow = "\x1B[43m";
-        public static String blue = "\x1B[44m";
-        public static String purple = "\x1B[45m";
-        public static String cyan = "\x1B[46m";
-        public static String white = "\x1B[47m";
+        // Text background colors
+        public static string black = "\x1B[40m";
+        public static string red = "\x1B[41m";
+        public static string green = "\x1B[42m";
+        public static string yellow = "\x1B[43m";
+        public static string blue = "\x1B[44m";
+        public static string purple = "\x1B[45m";
+        public static string cyan = "\x1B[46m";
+        public static string white = "\x1B[47m";
     }
 }
---
+++ lib/TextStyle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaleidolib.lib
+{
+    public class TextStyle
+    {
+        // Text Styles
+        public static string reset = "\x1B[0m";
+        public static string bold = "\x1B[1m";
+        public static string italic = "\x1B[3m";
+        public static string dim = "\x1B[2m";
+        public static string slowblink = "\x1B[5m"; // might not be supported in your terminal
+        public static string rapidblink = "\x1B[6m"; // rarely supported
+        public static string reverse = "\x1B[7m";
+        public static string invisible = "\x1B[8m"; // still able to be copied
+
+         // Text Decorations
+        public static string underline = "\x1B[4m";
+        public static string thickunderline = "\x1B[21m";
+        public static string italicunderline = "\x1B[4:3m";
+        public static string strikethrough = "\x1B[9m";
+        public static string overline = "\x1B[53m";
+    }
+}
